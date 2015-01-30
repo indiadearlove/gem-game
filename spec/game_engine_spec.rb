cb271aee0485082f7fd70c3a72e1cf78eaf97805
Jack Rubio <jackrubio26@gmail.com>
2015-01-30 13:11:10 +0000

    adding a colour change

--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -1,13 +1,15 @@
 require './lib/game_engine.rb'
+require './lib/grid.rb'
+require './lib/gem.rb'
 
 describe 'GameEngine' do
 
   let(:game){Game.new}
-  let(:grid){double :grid}
-  let(:jewel){double :jewel}
+  let(:g){Grid.new}
+  let(:jewel){Jewel.new}
 
-  it('should know if there are three gems of the same colour in a row') do
-    allow(game.jewel["A1", "A2", "A3"].colour).to eq "blue"
+  xit('should know if there are three gems of the same colour in a row') do
+    allow(grid["A1"].colours).to eq "blue"
     expect(game.jewel_row?).to eq true
   end
 
--- spec/gem_spec.rb
+++ spec/gem_spec.rb
@@ -9,4 +9,12 @@ describe 'Jewel' do
     expect(jewel.colour?).to eq true
   end
 
+  it('should be able to change colour') do
+    allow(jewel.colours).to eq "blue"
+    expect(jewel.colour).to eq "blue"
+    allow(jewel.change_colour!).to eq "red"
+    expect(jewel.colour).to eq "red"
+
+  end
+
 end
